7266f417f869e20cd9fca52b206e8f31df384afd
MrRari14 <ber.97ferrari@gmail.com>
2023-05-13 19:59:02 -0300

    Finishing header component for now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import CartLogo from "../../public/shopping-cart-svgrepo-com.svg";
- 
 
 export default function Header() {
     return (
         <>
-            <div className="bg-slate-200 fixed top-0 p-4 mb-4 w-full flex justify-between items-center">
-                <h3>Shop</h3>
+            <div className="bg-slate-200 font-bold text-lg fixed top-0 p-4 mb-4 w-full flex justify-between items-center">
+                <h3>bdki's</h3>
                 <h1>FOOTBALL SHOP</h1>
                 <div className="flex justify-between items-center">
-                    <CartLogo />
+                    <img src="cart-g92a6a8563_640.png" alt="cart" height={40} width={40}></img>
                     <span className="p-2">0</span>
                 </div>
             </div>
