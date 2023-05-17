d814ee1d9a9ede08c9be9a2a4954ff30cbd1bf17
MrRari14 <ber.97ferrari@gmail.com>
2023-05-17 10:31:36 -0300

    Removing add to cart button when its already in cart

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -14,12 +14,12 @@ const PlayerCard: React.FC<PlayerCardProps> = ({
     handleCartAdd, isInCart, handleCartRemove}) => {
     return (
         <>
-            <div className="flex flex-col items-center items-center m-2 p-2 bg-red-400 mw-80 mh-80">
+            <div className="flex flex-col items-center items-center m-2 p-2 bg-red-400 w-80 mh-80">
                 <h3 className="p-2">{playerName}</h3>
                 <img src={playerLogo} alt="clublogo" className="bg-blue-400 m-2 mh-full mw-full radius-50"></img>
                 <div className="w-full h-full flex justify-around items-center">
                     <span className="p-4">${playerPrice}</span>
-                    <button className="border border-black p-2" onClick={handleCartAdd}>Add to cart</button>
+                    { isInCart ? '' : <button className="border border-black p-2" onClick={handleCartAdd}>Add to cart</button> }
                     { isInCart && (<button className="p-2" onClick={handleCartRemove} ><img src="delete.png" alt="trashcan" width={40} height={30}></img></button> )}
                 </div>    
             </div>        
