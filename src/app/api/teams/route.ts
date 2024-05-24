ddbe29d80f9f9912ea53596121a543c5c38b2208
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-24 17:38:18 -0300

    chore: remove unused import

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { NextResponse } from "next/server"
 import randomizeTeam from "@/app/utils/randomTeam"
-import { data } from "autoprefixer"
   
 const randomTeam = randomizeTeam(80, 90)
 
