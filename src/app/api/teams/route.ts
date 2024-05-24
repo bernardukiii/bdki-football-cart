30c324ce95b8090bf1f9d6dce9217edcf2ccfa35
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-05-24 17:37:21 -0300

    fix: passing down the data in the route file

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { NextResponse } from "next/server"
 import randomizeTeam from "@/app/utils/randomTeam"
+import { data } from "autoprefixer"
   
 const randomTeam = randomizeTeam(80, 90)
 
@@ -9,7 +10,8 @@ export async function GET() {
     const response = await axios.get(`https://apiv2.allsportsapi.com/football/?&met=Teams&teamId=${randomTeam}&APIkey=${process.env.API_KEY}`)
     try {
         if (response.status === 200) {
-            return NextResponse.json({ status: response.status, message: 'The request was SUCCESSFULL in the backend!'})
+            console.log(response.data)
+            return NextResponse.json({ status: response.status, message: 'The request was SUCCESSFULL in the backend!', data: response.data})
         } else {
             return NextResponse.json({ status: response.status, message: 'The request FAILED in the backend :('})
         }
