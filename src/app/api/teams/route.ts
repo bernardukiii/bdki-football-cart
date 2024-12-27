9d549f3002aea6b7a54d2d638b4e6e2aedd18c6f
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-12-27 12:09:02 -0300

    chore: moving from AXIOS to FETCH

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -1,14 +1,14 @@
-import axios from "axios"
 import { NextResponse } from "next/server"
 import randomizeTeam from "@/app/utils/randomTeam"
   
 const randomTeam = randomizeTeam(80, 90)
 
 export async function GET() {
-    const response = await axios.get(`https://apiv2.allsportsapi.com/football/?&met=Teams&teamId=${randomTeam}&APIkey=${process.env.API_KEY}`)
+    const response = await fetch(`https://apiv2.allsportsapi.com/football/?&met=Teams&teamId=${randomTeam}&APIkey=${process.env.API_KEY}`)
+    const data = await response.json()
     try {
         if (response.status === 200) {
-            return NextResponse.json({ status: response.status, message: 'The request was SUCCESSFULL in the backend!', data: response.data})
+            return NextResponse.json({ status: response.status, message: 'The request was SUCCESSFULL in the backend!', data: data})
         } else {
             return NextResponse.json({ status: response.status, message: 'The request FAILED in the backend :('})
         }
