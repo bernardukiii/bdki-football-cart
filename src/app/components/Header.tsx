1ee9ec50b27938a928a7aa52c471da503462d689
Bernardo Ferrari <ber.97ferrari@gmail.com>
2025-02-12 15:46:47 -0300

    chore: testing

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ const Header:React.FC<HeaderProps> = ({cartTotal}) => {
         <>
             <div className="bg-green-900 border-b-8 border-green-950 text-white font-bold text-lg sticky top-0 p-4 mb-4 w-full flex justify-around items-center">
                 <h3 className="flex-grow-1">bdki&apos;s</h3>
-                <h1 className="flex-grow-2">MATI ES GAY</h1>
+                <h1 className="flex-grow-2">FOOTBALL</h1>
                 <div className="flex-grow-1 flex justify-between items-center">
                     <img src="icons8-cart-64.png" alt="cart" width={40} height={40}></img>
                     <span className="p-2 w-1">${cartTotal}</span>
